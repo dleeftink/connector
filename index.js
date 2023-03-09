da08f03057fd0d9bbe6be0c5bb5d3567877a14a9
dleeftink <dennisleeftink@gmail.com>
2023-03-09 14:26:47 +1000

    3 files modified

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const app = express();
 const port = 3010;
 const path = require('path');
 
+const axios = require('axios');
+const HttpsProxyAgent = require('https-proxy-agent');
+
 app.use(express.static('static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('pages/index.html'));
+app.get('/', async (req, res) => {
+  const proxyAgent = new HttpsProxyAgent('http://46.250.171.31:8080');
+  const response = await axios('https://api.ipify.org/?format=json', {
+   // agent: proxyAgent,
+  });
+  const body = await response.text();
+
+  res.send(JSON.stringify({ hello: 'world', body }));
 });
 
 app.listen(port, () => {
